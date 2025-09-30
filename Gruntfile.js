93e2cce14a04abdcf6419748cc56c45dd84413ed
mateuszmalkiewicz <mati.malkiewicz@gmail.com>
2025-09-30 15:29:22 +0200

    Fix: error, dunno what it is

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,6 +99,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Define tasks
     grunt.registerTask('lint', ['jshint:source']);
