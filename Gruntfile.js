d59423e4e0f908449dee1167bb1d0c0a5855ea29
mateuszmalkiewicz <mati.malkiewicz@gmail.com>
2025-09-30 03:26:47 +0200

    Make app not dependent on server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,21 @@
 module.exports = function(grunt) {
-    require('dotenv').config();
-    const sheet_id = process.env.SHEET_ID;
-    const api_key = process.env.GOOGLE_API;
-
     const sass = require('sass');
-    var rewrite = require('connect-modrewrite');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        // Lint JS
         jshint: {
             source: ['Gruntfile.js', 'source/scripts/{,*/}*.js', '!source/scripts/vendor/**'],
             test: ['test/**/*.js'],
-            options: {
-                esversion: 6
-            }
+            options: { esversion: 6 }
         },
+
+        // RequireJS optimizer
         requirejs: {
             options: {
                 appDir: "source",
-                baseUrl: ".", // relaive to appDir
+                baseUrl: ".",
                 dir: "build",
                 removeCombined: true,
                 modules: [{
@@ -26,9 +23,7 @@ module.exports = function(grunt) {
                     include: "requireLib"
                 }],
                 shim: {
-                    palette: {
-                        exports: 'palette'
-                    }
+                    palette: { exports: 'palette' }
                 },
                 paths: {
                     requireLib: '../node_modules/requirejs/require',
@@ -41,43 +36,31 @@ module.exports = function(grunt) {
                 },
                 fileExclusionRegExp: /.*\.(?:tpl|s[ac]ss)/,
             },
-            production: {
-                options: {
-                    optimizeCss: "standard",
-                }
-            },
-            development: {
-                options: {
-                    optimize: "none"
-                }
-            }
+            production: { options: { optimizeCss: "standard" } },
+            development: { options: { optimize: "none" } }
         },
+
+        // Template processing
         template: {
-            options: {},
             compile: {
                 options: {
                     data: {
-                        appTitle: 'Wheel of Fortune',
+                        appTitle: 'Losu losu',
                         baseUrl: '/',
-                        initialRoute: `wheel/sheet:losulosu`
+                        initialRoute: 'wheel/sheet:losulosu'
                     }
                 },
-                files: {
-                    'build/index.html': ['source/index.html.tpl']
-                }
+                files: { 'build/index.html': ['source/index.html.tpl'] }
             }
         },
+
+        // SCSS compilation
         sass: {
-            options: {
-                implementation: sass,
-                sourceMap: true
-            },
-            dist: {
-                files: {
-                    'build/style/style.css': 'source/style/style.scss'
-                }
-            }
+            options: { implementation: sass, sourceMap: true },
+            dist: { files: { 'build/style/style.css': 'source/style/style.scss' } }
         },
+
+        // CSS minification
         cssmin: {
             all: {
                 files: [{
@@ -89,75 +72,26 @@ module.exports = function(grunt) {
                 }]
             }
         },
-        connect: {
-            options: {
-                middleware: function(connect, options, middlewares) {
 
-                    var middleware = [];
-
-                    // 1. mod-rewrite behavior
-                    var rules = [
-                        '!\\.html|\\.js|\\.css|\\.svg|\\.jp(e?)g|\\.png|\\.gif$ /index.html'
-                    ];
-                    middleware.push(rewrite(rules));
-
-                    // 2. original middleware behavior
-                    var base = options.base;
-                    if (!Array.isArray(base)) {
-                        base = [base];
-                    }
-                    base.forEach(function(path) {
-                        middleware.push(require('serve-static')(path));
-                    });
-
-                    return middleware;
-                }
-            },
-            build: {
-                options: {
-                    hostname: 'localhost',
-                    port: 7878,
-                    base: 'build'
-                }
-            }
-        },
-        rsync: {
-            options: {
-                args: ["--verbose"],
-                recursive: true
-            },
-            production: {
-                options: {
-                    src: "build/",
-                    exclude: ["build.txt", ".DS_Store"],
-                    dest: "/tmp/wheel-of-fortune",
-                    host: "localhost",
-                    deleteAll: true
-                }
-            }
-        },
+        // QUnit tests
         qunit: {
             all: ['test/**/*.html']
         }
     });
 
+    // Load tasks
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-rsync');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-template');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
 
+    // Define tasks
     grunt.registerTask('lint', ['jshint:source']);
     grunt.registerTask('test', ['lint', 'jshint:test', 'qunit:all']);
     grunt.registerTask('build:development', ['lint', 'requirejs:development', 'template:compile', 'sass']);
     grunt.registerTask('build:production', ['lint', 'requirejs:production', 'template:compile', 'sass', 'cssmin:all']);
     grunt.registerTask('build', ['build:development']);
-    grunt.registerTask('deploy', ['build:production', 'rsync:production']);
-    grunt.registerTask('server', ['connect:build:keepalive']);
-    grunt.registerTask('build', ['build:development']);
-
     grunt.registerTask('default', ['build']);
 };
