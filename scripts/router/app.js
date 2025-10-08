b4faf9ae87ae8a47661ad295924c06fbb72ab274
matmal02 <matmal02@users.noreply.github.com>
2025-10-08 09:22:36 +0000

    deploy: 3c26f8199bab12f91fd1d5967025b73404b3c5e2

--- scripts/router/app.js
+++ scripts/router/app.js
@@ -34159,7 +34159,7 @@ define('scripts/views/wheel',["jquery", "moment", "underscore", "scripts/helper/
                 this.collection.on("reset", this.reset, this);
 
                 var rand = Math.floor(Math.random * 200) + 1;
-                if (r >= 150){
+                if (rand >= 150){
                     this.spinAudio = new Audio("/losu-losu-teleturniejow/sounds/spin2.mp3");
                 }
                 else
