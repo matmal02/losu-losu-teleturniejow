155c9154daaf8a2211b7ceb11bc234f16a5f181c
mateuszmalkiewicz <mati.malkiewicz@gmail.com>
2025-10-08 19:53:15 +0200

    feature: lower the chance of second audio drop

--- source/scripts/views/wheel.js
+++ source/scripts/views/wheel.js
@@ -35,8 +35,8 @@ define(["jquery", "moment", "underscore", "scripts/helper/math", "backbone", "sc
                 this.reset();
                 this.collection.on("reset", this.reset, this);
 
-                var rand = Math.floor(Math.random() * 200) + 1;
-                if (rand >= 150){
+                var rand = Math.floor(Math.random() * 1000) + 1;
+                if (rand > 150 && rand <= 200){
                     this.spinAudio = new Audio("/losu-losu-teleturniejow/sounds/spin2.mp3");
                 }
                 else
