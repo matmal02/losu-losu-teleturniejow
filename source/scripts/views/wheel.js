62500f083c12aa5f5780f12091926a9a555458c5
mateuszmalkiewicz <mati.malkiewicz@gmail.com>
2025-10-08 16:28:42 +0200

    quick fix

--- source/scripts/views/wheel.js
+++ source/scripts/views/wheel.js
@@ -35,7 +35,7 @@ define(["jquery", "moment", "underscore", "scripts/helper/math", "backbone", "sc
                 this.reset();
                 this.collection.on("reset", this.reset, this);
 
-                var rand = Math.floor(Math.random * 200) + 1;
+                var rand = Math.floor(Math.random() * 200) + 1;
                 if (rand >= 150){
                     this.spinAudio = new Audio("/losu-losu-teleturniejow/sounds/spin2.mp3");
                 }
